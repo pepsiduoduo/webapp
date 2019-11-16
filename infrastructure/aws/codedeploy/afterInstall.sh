d4d859f5a2675d353cf60cd2924995f3d31e641d
pengwei cui <49319032+pepsiduoduo@users.noreply.github.com>
2019-11-15 21:21:48 -0500

    Update afterInstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -12,3 +12,21 @@ sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
+
+sudo systemctl stop SpringBoot-1.0-SNAPSHOT
+
+sudo mv -f var/SpringBoot-1.0-SNAPSHOT.jar /var/SpringBoot-1.0-SNAPSHOT/
+
+sudo chown SpringBoot-1.0-SNAPSHOT:SpringBoot-1.0-SNAPSHOT /varSpringBoot-1.0-SNAPSHOT//SpringBoot-1.0-SNAPSHOT.jar
+
+sudo chmod 500 /var/SpringBoot-1.0-SNAPSHOT/SpringBoot-1.0-SNAPSHOT.jar
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
+sudo systemctl enable SpringBoot-1.0-SNAPSHOT
+
+sudo systemctl start SpringBoot-1.0-SNAPSHOT
