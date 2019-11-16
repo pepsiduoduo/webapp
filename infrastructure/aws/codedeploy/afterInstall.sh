545b271e066573218c947a3c8b19d1b7b316ffed
pengwei cui <49319032+pepsiduoduo@users.noreply.github.com>
2019-11-15 22:32:17 -0500

    Update afterInstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -3,14 +3,14 @@ sudo systemctl stop tomcat.service
 
 sudo systemctl restart tomcat.service
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+#sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
 
-sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+#sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
 # cleanup log files
-sudo rm -rf /opt/tomcat/logs/catalina*
-sudo rm -rf /opt/tomcat/logs/*.log
-sudo rm -rf /opt/tomcat/logs/*.txt
+#sudo rm -rf /opt/tomcat/logs/catalina*
+#sudo rm -rf /opt/tomcat/logs/*.log
+#sudo rm -rf /opt/tomcat/logs/*.txt
 
 sudo systemctl stop SpringBoot-1.0-SNAPSHOT
 
